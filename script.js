4853ead1b4b0c09997d473cb4d7a1a71deb5f280
H1roHamada <66208615+H1roHamada@users.noreply.github.com>
2021-02-28 08:55:50 +1000

    v 1.0.1
    v 1.0.1
    add timer for memorizing

--- script.js
+++ script.js
@@ -1,16 +1,81 @@
 import { cards } from "./db.js";
 const GAME = document.querySelector('.game')
+const START = document.querySelector('#start')
+const MAIN_SCREEN = document.querySelector('.mainScreen');
+const BTN_START = document.querySelector('.button');
 let CARD;
 let BACK;
 let FRONT;
-let activeCards = []
+let timer;
+let activeCards = [];
+let easy = 8;
+let medium = 18;
+let hard = 32;
+
+// difficulty
+let difficulty = medium
 
 renderRandomCards(cards);
+
+BTN_START.addEventListener('click', (btn) => {
+    timer = prompt('Время на запоминание в секундах, не более 60');
+    if (timer.length != 0) {
+        if (timer.replace(/\s/g, '').length === 0 || isNaN(timer)) {
+            alert('Нужно писать число!');
+        }
+        else if (timer <= 60) {
+            btn.target.remove()
+            GAME.insertAdjacentHTML('afterend', `<h1 class='timerWrap'><span class='timer'>${timer}</span></h1>`)
+            const TIMER = document.querySelector('.timer')
+            initializeClock(TIMER)
+            CARD.forEach(e => {
+                e.classList.add('rotate')
+                setTimeout(() => {
+                    e.classList.remove('rotate')
+                }, (timer + 1) * 1000);
+            })
+            setTimeout(() => {
+                checkCard()
+            }, 500);
+
+        } else {
+            alert('Введите число меньше 60');
+        }
+    } else {
+        alert('Повторите еще раз');
+    }
+
+
+
+})
+
+
+function initializeClock(TIMER) {
+    var secondsSpan = TIMER
+    function updateClock() {
+        secondsSpan.innerHTML = (timer);
+        if (timer > 0) {
+            timer--
+            console.log(timer)
+        } else {
+            TIMER.parentNode.remove()
+            clearInterval(timeinterval);
+        }
+    }
+
+    updateClock();
+    var timeinterval = setInterval(updateClock, 1000);
+}
+
+
+
 function renderRandomCards(arrayOfCards) {
-    let arr, max_number, i, unic, a;
+    let arr, i, unic, a;
     arr = []
-    max_number = 8;
-    while (arr.length < max_number) {
+    while (arr.length < difficulty) {
+        if (difficulty > cards.length) {
+            difficulty = cards.length
+        }
         do {
             unic = true;
             a = Math.floor(Math.random() * arrayOfCards.length);
@@ -23,7 +88,7 @@ function renderRandomCards(arrayOfCards) {
             }
         } while (!unic) // повторить генерацию числа
         arr.push(a);
-        if (arr.length == max_number) {
+        if (arr.length == difficulty) {
             let dblArr = arr.concat(arr);
             let AddForRender = shuffle(dblArr)
             for (let i = 0; i < dblArr.length; i++) {
@@ -38,7 +103,6 @@ function renderRandomCards(arrayOfCards) {
             CARD = document.querySelectorAll('.card')
             BACK = document.querySelectorAll('.back')
             FRONT = document.querySelectorAll('.front')
-            checkCard()
         }
     }
 }
@@ -62,6 +126,7 @@ function checkCard() {
     CARD.forEach(e => e.addEventListener('click', () => {
         e.classList.add('rotate')
         activeCards.push(e)
+        console.log(activeCards)
         if (activeCards.length == 2) {
             CARD.forEach(item => {
                 item.classList.add('hold')
